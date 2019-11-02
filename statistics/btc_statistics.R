51916698a39af0d978d96d21fb396478de0718f7
sebastihol <sebastihol@sebastihol.com>
2019-11-03 00:37:20 +0100

    Added autocorrelation

--- statistics/btc_statistics.R
+++ statistics/btc_statistics.R
@@ -31,10 +31,45 @@ btc.incremental.sd <- function() {
   return(sd.vector)
 }
 
+btc.acf <- function() {
+  btc_stream.init()
+  correlation.period <- 24
+  counter <- 1
+  available_values <- c()
+  mean.value <- 0
+  acf.variance.sum <- 0
+  acf.cor.sum <- 0
+  acf.vector <- c()
+  acf.cor.func <- function(mean.value, available_values) {
+    acf.cor.sum <- 0
+    start_index <- length(available_values) - correlation.period
+    for(i in start_index:length(available_values)) {
+      acf.cor.sum <- acf.cor.sum + ((available_values[counter] - mean.value) * (available_values[i] - mean.value))
+    }
+    return(acf.cor.sum)
+  }
+  while(!is.null(value <- btc_stream.next_value())) {
+    available_values[counter] <- value
+    if(counter > correlation.period) {
+      mean.value <- sum(available_values) / length(available_values)
+      acf.variance.sum <- acf.variance.sum + (available_values[counter] - mean.value)^2
+      acf.cor.sum <- acf.cor.func(mean.value, available_values)
+      acf.vector[counter] <- acf.cor.sum / acf.variance.sum
+    }
+    else {
+      acf.vector[counter] <- NaN
+    }
+    counter <- counter + 1
+  }
+  return(acf.vector)
+}
+
 plot.btc.non_window_statistics <- function() {
   plot(x=btc_data_stream$timestamp, y=btc_data_stream$size, main="BTC", xlab="timestamp", ylab="Block size")
   means <- btc.incremental.mean()
   lines(x=means, col="blue")
   sd <- btc.incremental.sd()
   plot(x=btc_data_stream$timestamp, y=sd, col="red", type = "l", xlab="timestamp", ylab="standard deviation")
+  acf <- btc.acf()
+  plot(acf, col="red", type = "l", ylab="autocorrelation")
 }
--- statistics/eth_statistics.R
+++ statistics/eth_statistics.R
@@ -31,10 +31,45 @@ eth.incremental.sd <- function() {
   return(sd.vector)
 }
 
+eth.acf <- function() {
+  eth_stream.init()
+  correlation.period <- 24
+  counter <- 1
+  available_values <- c()
+  mean.value <- 0
+  acf.variance.sum <- 0
+  acf.cor.sum <- 0
+  acf.vector <- c()
+  acf.cor.func <- function(mean.value, available_values) {
+    acf.cor.sum <- 0
+    start_index <- length(available_values) - correlation.period
+    for(i in start_index:length(available_values)) {
+      acf.cor.sum <- acf.cor.sum + ((available_values[counter] - mean.value) * (available_values[i] - mean.value))
+    }
+    return(acf.cor.sum)
+  }
+  while(!is.null(value <- eth_stream.next_value())) {
+    available_values[counter] <- value
+    if(counter > correlation.period) {
+      mean.value <- sum(available_values) / length(available_values)
+      acf.variance.sum <- acf.variance.sum + (available_values[counter] - mean.value)^2
+      acf.cor.sum <- acf.cor.func(mean.value, available_values)
+      acf.vector[counter] <- acf.cor.sum / acf.variance.sum
+    }
+    else {
+      acf.vector[counter] <- NaN
+    }
+    counter <- counter + 1
+  }
+  return(acf.vector)
+}
+
 plot.eth.non_window_statistics <- function() {
   plot(x=eth_data_stream$timestamp, y=eth_data_stream$size, main="ETH", xlab="timestamp", ylab="Block size")
   means <- eth.incremental.mean()
   lines(x=means, col="blue")
   sd <- eth.incremental.sd()
   plot(x=eth_data_stream$timestamp, y=sd, col="red", type = "l", xlab="timestamp", ylab="standard deviation")
+  acf <- eth.acf()
+  plot(acf, col="red", type = "l", ylab="autocorrelation")
 }
